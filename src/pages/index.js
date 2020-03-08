c172087fee0aacdbeddf905a6cceeace73e74bf3
Nakanishi, Yusuke <nakanishiyusuke0000@gmail.com>
2020-03-08 18:19:07 +0900

    チュートリアル記事を追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,32 @@
 import React from "react"
 import { Link } from "gatsby"
 
-const IndexPage = () => <div>Hasura Japan User Group</div>
+const IndexPage = () => (
+  <div>
+    <h2>Hasura Japan User Group</h2>
+    <ol>
+      <li>
+        <a href="https://uncle-javascript.com/hasura-tutorial-1">
+          Hasura チュートリアル1 Hasura を立ち上げよう
+        </a>
+      </li>
+      <li>
+        <a href="https://uncle-javascript.com/hasura-tutorial-2">
+          Hasura チュートリアル2 データベース(PostgresDB)にテーブルを作ろう
+        </a>
+      </li>
+      <li>
+        <a href="https://uncle-javascript.com/hasura-tutorial-3">
+          Hasura チュートリアル3 GraphQL で DB から値を取得する
+        </a>
+      </li>
+      <li>
+        <a href="https://uncle-javascript.com/hasura-tutorial-4">
+          Hasura チュートリアル4 GraphQL で DB に item を追加する
+        </a>
+      </li>
+    </ol>
+  </div>
+)
 
 export default IndexPage
