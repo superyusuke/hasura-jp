8737da0d7e6be5e3a56d8f64d4212fe3a9c019cd
Nakanishi, Yusuke <nakanishiyusuke0000@gmail.com>
2020-07-20 17:48:16 +0900

    記事更新

---
+++ src/components/HasuraMeetup1.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export const HasuraMeetup1 = () => (
+  <div>
+    <h3>HasuraMeetup1</h3>
+    <ol>
+      <li>
+        <a href="https://uncle-javascript.com/hasura-tutorial-1">
+          やりました
+        </a>
+      </li>
+    </ol>
+  </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
+import { HasuraTutorial } from "src/components/HasuraTutorial"
+import { HasuraMeetup1 } from "src/components/HasuraMeetup1"
 
 const IndexPage = () => (
   <div>
     <h2>Hasura Japan User Group</h2>
-    <h3>Hasura First Tutorial</h3>
-    <ol>
-      <li>
-        <a href="https://uncle-javascript.com/hasura-tutorial-1">
-          Hasura チュートリアル1 Hasura を立ち上げよう
-        </a>
-      </li>
-      <li>
-        <a href="https://uncle-javascript.com/hasura-tutorial-2">
-          Hasura チュートリアル2 データベース(PostgresDB)にテーブルを作ろう
-        </a>
-      </li>
-      <li>
-        <a href="https://uncle-javascript.com/hasura-tutorial-3">
-          Hasura チュートリアル3 GraphQL で DB から値を取得する
-        </a>
-      </li>
-      <li>
-        <a href="https://uncle-javascript.com/hasura-tutorial-4">
-          Hasura チュートリアル4 GraphQL で DB に item を追加する
-        </a>
-      </li>
-      <li>
-        <a href="https://uncle-javascript.com/hasura-tutorial-5">
-          Hasura チュートリアル5 Hasura の全体像について理解しよう
-        </a>
-      </li>
-    </ol>
+    <HasuraMeetup1 />
+    <HasuraTutorial />
   </div>
 )
 
